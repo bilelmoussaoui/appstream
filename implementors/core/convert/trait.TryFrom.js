73f56419ce554da469a33fd7f88472ce757c8144
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2020-08-12 18:39:27 +0000

    Automated deployment: Wed Aug 12 18:39:27 UTC 2020 9232ef5fcaeade6f5888e51de78bf28835500e82

--- implementors/core/convert/trait.TryFrom.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["appstream_rs"] = [{"text":"impl&lt;'_&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;'_ <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"appstream_rs/struct.AppId.html\" title=\"struct appstream_rs::AppId\">AppId</a>","synthetic":false,"types":["appstream_rs::app_id::AppId"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
