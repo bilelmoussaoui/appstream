8f9b31d0e6d6a659f33e371521b6a5de99a58fac
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2020-08-13 01:42:37 +0000

    Automated deployment: Thu Aug 13 01:42:37 UTC 2020 d128f97bd5d9478e65989e0728d70681ad65a556

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["appstream_rs"] = {"name":"","files":["app_id.rs","builders.rs","collection.rs","component.rs","content_rating.rs","de.rs","enums.rs","language.rs","lib.rs","license.rs","release.rs","screenshot.rs","translatable_string.rs"]};
+sourcesIndex["appstream"] = {"name":"","files":["app_id.rs","builders.rs","collection.rs","component.rs","content_rating.rs","de.rs","enums.rs","language.rs","lib.rs","license.rs","release.rs","screenshot.rs","translatable_string.rs"]};
 createSourceSidebar();
