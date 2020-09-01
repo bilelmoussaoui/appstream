cad3824df6020f55ea7427fede04f8aa3f817519
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2020-09-01 23:23:12 +0000

    Automated deployment: Tue Sep  1 23:23:12 UTC 2020 4d234b5e40314635dd0e73ad4531bfc5bf249bfb

---
+++ implementors/xmltree/trait.ElementPredicate.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["xmltree"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
