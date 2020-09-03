515304ee1577e421fb58c6a144b511a7ea371e75
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2020-09-03 16:01:37 +0000

    Automated deployment: Thu Sep  3 16:01:37 UTC 2020 7a70651740994da50e98e2c71efbd85ea7dfd559

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["appstream"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"appstream/enum.ParseError.html\" title=\"enum appstream::ParseError\">ParseError</a>","synthetic":false,"types":["appstream::error::ParseError"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
