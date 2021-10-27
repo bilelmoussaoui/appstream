4c390d41735334b7a2ea1ba56d1455039e88d796
bilelmoussaoui <bilelmoussaoui@users.noreply.github.com>
2021-10-27 15:26:42 +0000

    Automated deployment: Wed Oct 27 15:26:42 UTC 2021 f63e1ab4442ac4d8c471d3279ec168bed8d84057

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["appstream"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"appstream/enum.ParseError.html\" title=\"enum appstream::ParseError\">ParseError</a>","synthetic":false,"types":["appstream::error::ParseError"]}];
+implementors["appstream"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.0/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"appstream/enum.ParseError.html\" title=\"enum appstream::ParseError\">ParseError</a>","synthetic":false,"types":["appstream::error::ParseError"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
